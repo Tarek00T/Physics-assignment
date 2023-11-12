c6c6fc4eda1fa01f14bcf5920a909815449bf677
Tarek <140520331+Tarek00T@users.noreply.github.com>
2023-11-12 16:03:05 +0600

    Add files via upload

---
+++ physics problem 2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    //angle theta=45 degree
+    //Order n=1;
+    //d/sin(theta)==sqrt(2);
+    double m=sqrt(2);
+
+    printf("\nRatio between slits width and wavelength must =%.6lf\n",m);
+
+    return 0;
+}
